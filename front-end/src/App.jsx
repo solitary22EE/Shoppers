506048396f063cc137a31d045798cf21c4ce63e1
solitary22EE <rajkumarkarikkal8942@gmail.com>
2025-07-25 16:41:18 +0530

    first commit

---
+++ front-end/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import RouterFile from './Router/RouterFile'
+
+const App = () => {
+  return (
+    <div>
+      <RouterFile/>
+    </div>
+  )
+}
+
+export default App
---
+++ front-end/src/Components/Products/BigSale.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BigSale = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default BigSale
