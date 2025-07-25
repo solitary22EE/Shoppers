10d0eb59b12030730fbb6ca280163cd55be33dba
solitary22EE <rajkumarkarikkal8942@gmail.com>
2025-07-25 17:04:04 +0530

    Initial commit of full MERN stack project

---
+++ front-end/src/Components/Products/BigSale.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BigSale = () => {
+  return (
+    <div>
+      Hai this is the bigsale page
+    </div>
+  )
+}
+
+export default BigSale
---
+++ front-end/src/Router/RouterFile.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/navbar/Navbar'
+import Hero from '../Components/Hero/Hero'
+import Featured from '../Components/Products/Featured'
+const RouterFile = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero/>
+      <Featured/>
+    </div>
+  )
+}
+
+export default RouterFile
